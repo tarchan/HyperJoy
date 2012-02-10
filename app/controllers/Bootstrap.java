bea898feaf2b9ac1b36f0afff42b44568f91e719
tarchan <tarchan@gmail.com>
2012-02-11 02:21:15 +0900

    add bootstrap

---
+++ app/controllers/Bootstrap.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
+
+@OnApplicationStart
+public class Bootstrap extends Job
+{
+	static final String HYPERJOY = "http://homepage1.nifty.com/yottoide/hyperjoy.html";
+
+	public void doJob()
+	{
+		// Check if the database is empty
+//		if (User.count() == 0)
+//		{
+//			Fixtures.loadModels("initial-data.yml");
+//		}
+		if (Song.count() == 0)
+		{
+			Song.load(HYPERJOY);
+		}
+	}
+}
